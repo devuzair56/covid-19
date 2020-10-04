c85b01c4aabea595b6610a94015ebaf95eb80e50
devuzair56 <72253079+devuzair56@users.noreply.github.com>
2020-10-04 20:21:34 +0500

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+       Developed by uzair iftikhar
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
